6daf7baba7badc7550cf69c883fdd8f0c615c93c
Elvin <elvinaa@code.edu.az>
2022-04-24 17:03:47 +0400

    last version

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,25 +20,50 @@ async function apiFunc() {
 first_side_btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         let btn_checked_btn_for_value_change=e.target.innerText
+        let second_side_checked_btn=document.querySelector('.checked_btn2').innerText
         document.querySelector('.checked_btn1').classList.remove('checked_btn1')
         e.target.classList.add('checked_btn1')
         if (firstImpt.value) {
-            // secondimpt.value=
             apiFunc().then(res => {
-                //secondimpt.value=res.rates[btn_checked_btn_for_value_change];
-                console.log(firstImpt.value*res.rates[`${btn_checked_btn_for_value_change}`])
+                secondimpt.value=res.rates[`${second_side_checked_btn}`]
+                /
+                res.rates[`${btn_checked_btn_for_value_change}`]*firstImpt.value
+            })
+        }
+    })
+})
+second_side_btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        let btn_checked_btn_for_value_change=e.target.innerText
+        let first_side_checked_btn=document.querySelector('.checked_btn1').innerText
+        document.querySelector('.checked_btn2').classList.remove('checked_btn2')
+        e.target.classList.add('checked_btn2')
+        if (secondimpt.value) {
+            apiFunc().then(res => {
+                firstImpt.value=res.rates[`${first_side_checked_btn}`]
+                /
+                res.rates[`${btn_checked_btn_for_value_change}`]*secondimpt.value
             })
         }
     })
 })
 
-second_side_btns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        document.querySelector('.checked_btn2').classList.remove('checked_btn2')
-        e.target.classList.add('checked_btn2')
-        if (secondimpt.value) {
-            // firstImpt.value=
 
-        }
+firstImpt.addEventListener('input',(e)=>{
+    let first_side_checked_btn = document.querySelector('.checked_btn1').innerText
+    let second_side_checked_btn = document.querySelector('.checked_btn2').innerText
+
+    apiFunc().then((res)=>{
+        secondimpt.value=res.rates[`${second_side_checked_btn}`]/res.rates[`${first_side_checked_btn}`]*e.target.value;
+        
+    })
+})
+
+secondimpt.addEventListener('input',(e)=>{
+    let first_side_checked_btn = document.querySelector('.checked_btn1').innerText
+    let second_side_checked_btn = document.querySelector('.checked_btn2').innerText
+
+    apiFunc().then((res)=>{
+        firstImpt.value=res.rates[`${first_side_checked_btn}`]/res.rates[`${second_side_checked_btn}`]*e.target.value;
     })
 })
