3bcad30200b1f6db0d9e2f99e9d69e92d11be046
Elvin <elvinaa@code.edu.az>
2022-05-04 16:08:40 +0400

    initial last with equal sides cant use fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ let firstImpt = document.querySelector('#first_impt');
 let secondimpt = document.querySelector('#second_impt');
 let first_side_btns = document.querySelectorAll('.first_side_btns')
 let second_side_btns = document.querySelectorAll('.second_side_btns')
-let bottom_side_of_currency_first=document.querySelector('.first_bottom')
-let bottom_side_of_currency_second=document.querySelector('.second_bottom')
+let bottom_side_of_currency_first = document.querySelector('.first_bottom')
+let bottom_side_of_currency_second = document.querySelector('.second_bottom')
 const usd_value = 1;
 var obj;
 
@@ -21,69 +21,87 @@ async function apiFunc() {
 
 first_side_btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        let btn_checked_btn_for_value_change=e.target.innerText
-        let second_side_checked_btn=document.querySelector('.checked_btn2').innerText
-        document.querySelector('.checked_btn1').classList.remove('checked_btn1')
-        e.target.classList.add('checked_btn1')
-        if (firstImpt.value) {
+        let btn_checked_btn_for_value_change = e.target.innerText
+        let second_side_checked_btn = document.querySelector('.checked_btn2').innerText
+
+        if (btn_checked_btn_for_value_change != second_side_checked_btn) {
+            document.querySelector('.checked_btn1').classList.remove('checked_btn1')
+            e.target.classList.add('checked_btn1')
+            if (firstImpt.value) {
+                apiFunc().then(res => {
+                    secondimpt.value = res.rates[`${second_side_checked_btn}`]
+                        /
+                        res.rates[`${btn_checked_btn_for_value_change}`] * firstImpt.value
+                })
+            }
             apiFunc().then(res => {
-                secondimpt.value=res.rates[`${second_side_checked_btn}`]
-                /
-                res.rates[`${btn_checked_btn_for_value_change}`]*firstImpt.value
+                bottom_side_of_currency_first.innerText = `1 ${e.target.innerText}`
+                    + " = " + `${(res.rates[second_side_checked_btn] / res.rates[e.target.innerText]).toFixed(4)}`
+                    + `${second_side_checked_btn}`
+                bottom_side_of_currency_second.innerText = `1 ${second_side_checked_btn}`
+                    + ` = `
+                    + `${(res.rates[e.target.innerText] / res.rates[second_side_checked_btn]).toFixed(4)}`
+                    + ` ${e.target.innerText}`
             })
+        } else {
+            alert("change")
         }
-        apiFunc().then(res => {
-            bottom_side_of_currency_first.innerText=`1 ${e.target.innerText}` 
-            + " = " + `${(res.rates[second_side_checked_btn]/res.rates[e.target.innerText]).toFixed(4)}`
-            +`${second_side_checked_btn}`
-            bottom_side_of_currency_second.innerText=`1 ${second_side_checked_btn}`
-            + ` = `
-            + `${(res.rates[e.target.innerText]/res.rates[second_side_checked_btn]).toFixed(4)}`
-            + ` ${e.target.innerText}`
-        })
-        
+
     })
 })
 second_side_btns.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-        let btn_checked_btn_for_value_change=e.target.innerText
-        let first_side_checked_btn=document.querySelector('.checked_btn1').innerText
-        document.querySelector('.checked_btn2').classList.remove('checked_btn2')
-        e.target.classList.add('checked_btn2')
-        if (secondimpt.value) {
+        let btn_checked_btn_for_value_change = e.target.innerText
+        let first_side_checked_btn = document.querySelector('.checked_btn1').innerText
+
+        if (btn_checked_btn_for_value_change != first_side_checked_btn) {
+            document.querySelector('.checked_btn2').classList.remove('checked_btn2')
+            e.target.classList.add('checked_btn2')
+            if (secondimpt.value) {
+                apiFunc().then(res => {
+                    firstImpt.value = res.rates[`${first_side_checked_btn}`]
+                        /
+                        res.rates[`${btn_checked_btn_for_value_change}`] * secondimpt.value
+                })
+            }
             apiFunc().then(res => {
-                firstImpt.value=res.rates[`${first_side_checked_btn}`]
-                /
-                res.rates[`${btn_checked_btn_for_value_change}`]*secondimpt.value
+                bottom_side_of_currency_second.innerText = `1 ${e.target.innerText}` + " = "
+                    + `${(res.rates[first_side_checked_btn] / res.rates[e.target.innerText]).toFixed(4)} `
+                    + `${first_side_checked_btn}`
+                bottom_side_of_currency_first.innerText = `1 ${first_side_checked_btn} `
+                    + `= ` + `${(res.rates[e.target.innerText] / res.rates[first_side_checked_btn]).toFixed(4)}`
+                    + `${e.target.innerText}`
             })
+        } else {
+            alert("change")
         }
-        apiFunc().then(res => {
-            bottom_side_of_currency_second.innerText = `1 ${e.target.innerText}` + " = " 
-            +`${(res.rates[first_side_checked_btn]/res.rates[e.target.innerText]).toFixed(4)} ` 
-            + `${first_side_checked_btn}`
-            bottom_side_of_currency_first.innerText = `1 ${first_side_checked_btn} `
-            + `= `+ `${(res.rates[e.target.innerText]/res.rates[first_side_checked_btn]).toFixed(4)}`
-            + `${e.target.innerText}`
+    })
+})
+
+
+firstImpt.addEventListener('input', (e) => {
+    let first_side_checked_btn = document.querySelector('.checked_btn1').innerText
+    let second_side_checked_btn = document.querySelector('.checked_btn2').innerText
+
+    if (first_side_checked_btn != second_side_checked_btn) {
+        apiFunc().then((res) => {
+            secondimpt.value = res.rates[`${second_side_checked_btn}`] / res.rates[`${first_side_checked_btn}`] * e.target.value;
+
         })
-    })
+    } else {
+        alert("change")
+    }
 })
 
-
-firstImpt.addEventListener('input',(e)=>{
+secondimpt.addEventListener('input', (e) => {
     let first_side_checked_btn = document.querySelector('.checked_btn1').innerText
     let second_side_checked_btn = document.querySelector('.checked_btn2').innerText
 
-    apiFunc().then((res)=>{
-        secondimpt.value=res.rates[`${second_side_checked_btn}`]/res.rates[`${first_side_checked_btn}`]*e.target.value;
-        
-    })
-})
-
-secondimpt.addEventListener('input',(e)=>{
-    let first_side_checked_btn = document.querySelector('.checked_btn1').innerText
-    let second_side_checked_btn = document.querySelector('.checked_btn2').innerText
-
-    apiFunc().then((res)=>{
-        firstImpt.value=res.rates[`${first_side_checked_btn}`]/res.rates[`${second_side_checked_btn}`]*e.target.value;
-    })
+    if (first_side_checked_btn != second_side_checked_btn) {
+        apiFunc().then((res) => {
+            firstImpt.value = res.rates[`${first_side_checked_btn}`] / res.rates[`${second_side_checked_btn}`] * e.target.value;
+        })
+    } else {
+        alert("change")
+    }
 })
